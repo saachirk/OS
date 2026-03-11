2772c9c4abfa3b6fbb35ca693fe02ae692710fd8
Saachi R Katti <saachirkatti.cs24@bmsce.ac.in>
2026-03-11 12:36:01 +0530

    LAB PROGRAM 3

---
+++ LAB PROGRAM 3/priority_nonpreemptive.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    int pid[n], at[n], bt[n], pr[n];
+    int ct[n], tat[n], wt[n], rt[n];
+    int completed[n];
+
+    for(int i = 0; i < n; i++) {
+        printf("\nProcess %d\n", i+1);
+
+        printf("Enter PID: ");
+        scanf("%d", &pid[i]);
+
+        printf("Enter Arrival Time (AT): ");
+        scanf("%d", &at[i]);
+
+        printf("Enter Burst Time (BT): ");
+        scanf("%d", &bt[i]);
+
+        printf("Enter Priority: ");
+        scanf("%d", &pr[i]);
+
+        completed[i] = 0;
+    }
+
+    int time = 0, done = 0;
+
+    while(done < n) {
+        int idx = -1;
+        int highest_priority = INT_MAX;
+
+        for(int i = 0; i < n; i++) {
+            if(at[i] <= time && completed[i] == 0) {
+                if(pr[i] < highest_priority) {
+                    highest_priority = pr[i];
+                    idx = i;
+                }
+            }
+        }
+
+        if(idx != -1) {
+
+            rt[idx] = time - at[idx];
+
+            time += bt[idx];
+            ct[idx] = time;
+
+            tat[idx] = ct[idx] - at[idx];
+            wt[idx] = tat[idx] - bt[idx];
+
+            completed[idx] = 1;
+            done++;
+        } 
+        else {
+            time++;
+        }
+    }
+
+    printf("\nPID\tAT\tBT\tPR\tCT\tTAT\tWT\tRT\n");
+
+    for(int i = 0; i < n; i++) {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+               pid[i], at[i], bt[i], pr[i],
+               ct[i], tat[i], wt[i], rt[i]);
+    }
+
+    return 0;
+}
